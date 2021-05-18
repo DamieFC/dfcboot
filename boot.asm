ab748537b72ca5b336d24868e0126ff02f9d9a27
GNUOverYooouu <dtortoise99@gmail.com>
2021-05-18 14:31:59 -0700

    Update boot.asm

--- boot.asm
+++ boot.asm
@@ -2,22 +2,22 @@
 ; Bootloader adapted from Babystep tutorials on OSDev.org using BIOS.
 
 mov ax, 0x07c0
-	mov ds, ax
+    mov ds, ax
 
-	mov si, msg
-	cld
+    mov si, msg
+    cld
 ch_loop:lodsb
-	or al, al ; zero=end or str
-	jz hang   ; get out
-	mov ah, 0x0E
-	mov bh, 0
-	int 0x10
-	jmp ch_loop
+    or al, al ; zero=end or str
+    jz hang   ; get out
+    mov ah, 0x0E
+    mov bh, 0
+    int 0x10
+    jmp ch_loop
 
 hang:
-	jmp hang
+    jmp hang
 
-msg   db 'BIOS booted! Hello!', 13, 10, 0
-	times 512-($-$$) db 0 ; I changed it from 510 to 512
-	db 0x55
-	db 0xAA
+msg   db 'DFCBOOT BIOS booted! Hello!', 13, 10, 0
+    times 512-($-$$) db 0 ; I changed it from 510 to 512
+    db 0x55
+    db 0xAA
